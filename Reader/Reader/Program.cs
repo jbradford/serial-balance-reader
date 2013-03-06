7450ba36581db61dd54f46a9b240fa3612b8c45a
Jon Bradford <jon.bradford@bathasu.com>
2013-03-06 15:51:56 +0000

    Created basic project.

---
+++ Reader/Reader/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ports = SerialPort.GetPortNames();
+            foreach (var port in ports)
+            {
+                Console.WriteLine(port);
+            }
+            Console.ReadLine();
+        }
+    }
+}
