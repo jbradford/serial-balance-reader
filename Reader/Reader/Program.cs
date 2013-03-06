61576c8fd36d47ee325d3dbdeeba8c90e3bf869c
Jon Bradford <jon.bradford@bathasu.com>
2013-03-06 16:00:19 +0000

    Receives Messages

--- Reader/Reader/Program.cs
+++ Reader/Reader/Program.cs
@@ -11,12 +11,25 @@ namespace Reader
     {
         static void Main(string[] args)
         {
-            var ports = SerialPort.GetPortNames();
-            foreach (var port in ports)
-            {
-                Console.WriteLine(port);
-            }
-            Console.ReadLine();
+            var port = new SerialPort("/dev/ttyUSB0", 2400, Parity.Even, 7) {Handshake = Handshake.XOnXOff};
+            port.DataReceived += DataReceivedHandler;
+
+            port.Open();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine();
+            Console.ReadKey();
+            port.Close();
+        }
+
+        private static void DataReceivedHandler(
+                            object sender,
+                            SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.WriteLine("Data Received:");
+            Console.Write(indata);
         }
     }
 }
