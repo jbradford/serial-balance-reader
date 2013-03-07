2392e65d63ecace33515a25d8a2eb0f7b5c2df22
Jon Bradford <jon.bradford@bathasu.com>
2013-03-07 09:31:22 +0000

    Now polls for data.

--- Reader/Reader/Program.cs
+++ Reader/Reader/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using EventStore.ClientAPI;
 using Newtonsoft.Json;
 
@@ -22,12 +19,16 @@ namespace Reader
             esCon = EventStoreConnection.Create();
             esCon.Connect(new IPEndPoint(IPAddress.Parse("10.3.1.103"),1113));
 
-            port.DataReceived += DataReceivedHandler;
+            //port.DataReceived += DataReceivedHandler;
             port.Open();
+
+            var readerThread = new Thread(ReceivedBalanceReading);
+            readerThread.Start(port);
 
             Console.WriteLine("Press any key to continue...");
             Console.WriteLine();
             Console.ReadKey();
+            readerThread.Abort();
             port.Close();
         }
 
@@ -53,6 +54,46 @@ namespace Reader
             var bytes = Encoding.UTF8.GetBytes(json);
             esCon.AppendToStreamAsync("balanceevents", ExpectedVersion.Any,
                                       new EventData(Guid.NewGuid(), "BalanceRead", true, bytes, new byte[] {}));
+        }
+
+        private static void ReceivedBalanceReading(object o)
+        {
+            var port = (SerialPort) o;
+            while (true)
+            {
+                string indata = ReadLine(port);
+                string indata2 = ReadLine(port);
+                string indata3 = ReadLine(port);
+
+                var readEvent = new BalanceRead();
+                readEvent.BalanceId = indata.Split(':')[1].Trim();
+                readEvent.ProjectId = indata2.Split(':')[1].Trim();
+                readEvent.Reading = Decimal.Parse(indata3.Substring(0, 8).Trim());
+                readEvent.Unit = indata3.Substring(8, 2).Trim();
+                readEvent.Stable = !indata3.Contains("?");
+
+                Console.WriteLine(readEvent.ToString());
+
+                var json = JsonConvert.SerializeObject(readEvent);
+                var bytes = Encoding.UTF8.GetBytes(json);
+                esCon.AppendToStreamAsync("balanceevents", ExpectedVersion.Any,
+                                          new EventData(Guid.NewGuid(), "BalanceRead", true, bytes, new byte[] {}));
+            }
+        }
+
+        private static string ReadLine(SerialPort port)
+        {
+            var rxString = "";
+
+            var tmpByte = (byte)port.ReadByte();
+
+            while (tmpByte != 13)
+            {
+                rxString += ((char)tmpByte);
+                tmpByte = (byte)port.ReadByte();
+            }
+
+            return rxString;
         }
     }
 
